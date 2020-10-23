65daed7c067bd3e6c830d01374d3ce958ed1c9ac
Jiang, Zihao <zihjiang@ebay.com>
2020-10-22 19:53:43 -0700

    Add host argument like macos csshx. Fix commands with -t

--- csshx.sh
+++ csshx.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 addresses=""
+hostfile=
 while [ "$#" -gt 0 ]; do
   case "$1" in
     -l) login="$2"; shift 2;;
@@ -8,10 +9,20 @@ while [ "$#" -gt 0 ]; do
     --login=*) login="${1#*=}"; shift 1;;
     --login) echo "$1 requires an argument" >&2; exit 1;;
 
+    -h) hostfile="$2"; shift 2;;
+
+    --host=*) hostfile="${1#*=}"; shift 1;;
+    --host) echo "$1 requires an argument" >&2; exit 1;;
+
     -*) echo "unknown option: $1" >&2; exit 1;;
     *) addresses=$(echo "$addresses $1"); shift 1;;
   esac
 done
+
+if [ ! -z "$hostfile" ] && [ -e "$hostfile" ]; then
+    addresses="$addresses $(cat $hostfile)"
+fi
+echo "Connecting to $addresses..."
 
 num_addresses=$(echo "$addresses" | wc -w)
 if [ "$num_addresses" -eq 0 ]; then
@@ -32,11 +43,11 @@ tmux new-window -n "ssh"
 i=0
 for addr in $(echo "$addresses"); do
     if [ "$i" -gt 0 ]; then
-        tmux split-window
-        tmux select-layout tiled
+        tmux split-window -t ssh
+        tmux select-layout -t ssh tiled
     fi
-    tmux send-keys "$(ssh_command $addr)" C-m
+    tmux send-keys -t ssh "$(ssh_command $addr)" C-m
     i=$((i+1))
 done
 
-tmux setw synchronize-panes
+tmux setw -t ssh synchronize-panes
